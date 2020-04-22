c5107c5cfe6899ba407b504e14a4ee5500ee0e26
Heorhii Zhemela <Heorhii.Zhemela@henryschein.com>
2020-04-22 21:58:57 +0300

    initial commit

---
+++ src/test/java/com/example/dataparser/DataparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
